5a341e59db05dbf6b11d4b02a9e5e067d83fecf7
workshop2 <simon@colmer.me>
2019-03-24 16:03:24 +0000

    Stroe http headers

--- src/OffalBot.Functions/InboundGithubWebhook.cs
+++ src/OffalBot.Functions/InboundGithubWebhook.cs
@@ -1,9 +1,12 @@
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OffalBot.Functions
 {
@@ -15,7 +18,16 @@ namespace OffalBot.Functions
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "github/webhooks")]HttpRequest req,
             ILogger log)
         {
-            return await new StreamReader(req.Body).ReadToEndAsync();
+            var payLoad = await new StreamReader(req.Body).ReadToEndAsync();
+            var jsonObject = JObject.Parse(payLoad);
+
+            var headers = req.Headers
+                .Select(x => new { key = x.Key, value = x.Value.FirstOrDefault() })
+                .ToDictionary(x => x.key, y => y.value);
+
+            jsonObject["httpHeaders"] = JObject.FromObject(headers);
+
+            return JsonConvert.SerializeObject(jsonObject);
         }
     }
 }
