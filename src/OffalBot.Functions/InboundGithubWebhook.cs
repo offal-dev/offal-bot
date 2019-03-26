d3582dd1460dbddddad691f1b9b6c940d8aad9c3
workshop2 <simon@colmer.me>
2019-03-26 23:00:50 +0000

    Temp function to move already collected data into own queues

--- src/OffalBot.Functions/InboundGithubWebhook.cs
+++ src/OffalBot.Functions/InboundGithubWebhook.cs
@@ -4,9 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Queue;
 using Newtonsoft.Json;
@@ -19,8 +17,8 @@ namespace OffalBot.Functions
         [FunctionName("inbound-github-webhook")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "github/webhooks")]HttpRequest req,
-            ILogger log,
-            ExecutionContext context)
+            ExecutionContext context,
+            ILogger log)
         {
             var payLoad = await new StreamReader(req.Body).ReadToEndAsync();
             var jsonObject = JObject.Parse(payLoad);
