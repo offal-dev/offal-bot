415ebe28ec25b3610b91393ba9753c23fdc854b0
workshop2 <simon@colmer.me>
2019-04-05 18:35:03 +0100

    Add function to migrate 10 queue items from poison at a time

---
+++ src/OffalBot.Functions/TempPoisonMigration.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace OffalBot.Functions
+{
+    public static class TempPoisonMigration
+    {
+        [FunctionName("temp-deployment-poison-migration")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "deployment-poison-migration")] HttpRequest req,
+            ExecutionContext context,
+            ILogger log)
+        {
+            var poison = await new AzureStorage(context).GetQueue("github-deployment-poison");
+            var messages = await poison.GetMessagesAsync(10);
+
+            var deployment = await new AzureStorage(context).GetQueue("github-deployment");
+
+            foreach (var message in messages)
+            {
+                log.LogInformation("Migrating message...");
+                await deployment.AddMessageAsync(message);
+            }
+
+            return new OkResult();
+        }
+    }
+}
