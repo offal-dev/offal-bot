d64ae8717e8bffff130734f2cf4565d193fcf80c
Simon Colmer <simon@colmer.me>
2019-11-16 20:36:20 +0000

    Force rebuild/deploy

--- src/OffalBot.Functions/ApiFunctions/JwtTest.cs
+++ src/OffalBot.Functions/ApiFunctions/JwtTest.cs
@@ -35,6 +35,6 @@ namespace OffalBot.Functions.ApiFunctions
                 Formatting = Formatting.Indented,
                 ContractResolver = new CamelCasePropertyNamesContractResolver()
             });
-        }
+        } 
     }
 }
